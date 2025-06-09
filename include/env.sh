31a1ea56febbc577824d7812f2e27002cf37f1f4
Kirill Makurin <maiddaisuki@outlook.com>
2025-06-10 00:54:09 +0900

    build: use INCLUDE and LIB to control compiler and linker search paths
    Both cl.exe and clang-cl.exe use INCLUDE as default search path
    for header files.
    
    Both link.exe and lld-link.exe use LIB as default search path
    for libraries.
    
    Using INCLUDE and LIB has the following advantages:
    
    - No need to pass -I and -L flags to CPPFLAGS and LDFLAGS for
      configure-based packages.
    - This allows packages which use cmake or meson to locate header
      files and libraries installed to PREFIX and PROGRAMS_PREFIX.

--- include/env.sh
+++ include/env.sh
@@ -8,19 +8,15 @@ if ${_need_tools-true}; then
 	. ${dir_include}/tools.sh
 fi
 
-if [ ${save_PATH+y} ]; then
-	PATH=${save_PATH}
-fi
+# Restore values, if they were saved
+test ${save_INCLUDE+y} && INCLUDE=${save_INCLUDE}
+test ${save_LIB+y} && LIB=${save_LIB}
+test ${save_PATH+y} && PATH=${save_PATH}
 
+# Save current values
+save_INCLUDE=${INCLUDE}
+save_LIB=${LIB}
 save_PATH=${PATH}
-
-if [ ${stage} -eq 1 ]; then
-	PATH=${u_build_prefix}/bin:${PATH}
-elif [ ${stage} -eq 2 ]; then
-	PATH=${u_prefix}/bin:${u_build_prefix}/bin:${PATH}
-else # stage 3
-	PATH=${u_programs_prefix}/bin:${u_prefix}/bin:${PATH}
-fi
 
 # Help CMake find dependencies
 export CMAKE_PREFIX_PATH=
@@ -30,17 +26,29 @@ export PKG_CONFIG_LIBDIR=
 export PKG_CONFIG_PATH=
 
 if [ ${stage} -eq 1 ]; then
+	PATH=${u_build_prefix}/bin:${PATH}
+	INCLUDE="${build_prefix}/include;${INCLUDE}"
+	LIB="${build_prefix}/lib;${LIB}"
 	CMAKE_PREFIX_PATH="${build_prefix}"
 	PKG_CONFIG_LIBDIR="${build_prefix}/lib/pkgconfig;${build_prefix}/share/pkgconfig"
 elif [ ${stage} -eq 2 ]; then
+	PATH=${u_prefix}/bin:${u_build_prefix}/bin:${PATH}
+	INCLUDE="${PREFIX}/include;${build_prefix}/include;${INCLUDE}"
+	LIB="${PREFIX}/lib;${build_prefix}/lib;${LIB}"
 	CMAKE_PREFIX_PATH="${PREFIX};${build_prefix}"
 	PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig;${PREFIX}/share/pkgconfig"
 	PKG_CONFIG_PATH="${build_prefix}/lib/pkgconfig;${build_prefix}/share/pkgconfig"
 else # stage 3
+	PATH=${u_prefix}/bin:${PATH}
+	INCLUDE="${PREFIX}/include;${INCLUDE}"
+	LIB="${PREFIX}/lib;${LIB}"
 	CMAKE_PREFIX_PATH="${PREFIX}"
 	PKG_CONFIG_LIBDIR="${PREFIX}/lib/pkgconfig;${PREFIX}/share/pkgconfig"
 
-	if [ "${PREFIX}" != "${PROGRAMS_PREFIX}" ]; then
+	if [ ! "${PREFIX}" -ef "${PROGRAMS_PREFIX}" ]; then
+		PATH=${u_programs_prefix}/bin:${PATH}
+		INCLUDE="${PROGRAMS_PREFIX}/include;${INCLUDE}"
+		LIB="${PROGRAMS_PREFIX}/lib;${LIB}"
 		CMAKE_PREFIX_PATH="${CMAKE_PREFIX_PATH};${PROGRAMS_PREFIX}"
 		PKG_CONFIG_PATH="${PROGRAMS_PREFIX}/lib/pkgconfig;${PROGRAMS_PREFIX}/share/pkgconfig"
 	fi
--- include/flags.sh
+++ include/flags.sh
@@ -5,27 +5,6 @@
 #
 
 ##
-# Set header and library search paths
-#
-# NOTE: `-external` flag tells cl.exe to treat headers in specified directory
-# as *external* (similar to *system* in gcc)
-#
-
-_incpath=
-_libpath=
-
-if [ ${stage} = 1 ]; then
-	_incpath="-I${u_build_prefix}/include -external:I${build_prefix}/include -external:W0"
-	_libpath="-L${u_build_prefix}/lib"
-elif [ ${stage} = 2 ]; then
-	_incpath="-I${u_prefix}/include -external:I${PREFIX}/include -I${u_build_prefix}/include -external:I${build_prefix}/include -external:W0"
-	_libpath="-L${u_prefix}/lib -L${u_build_prefix}/lib"
-elif [ ${stage} = 3 ]; then
-	_incpath="-I${u_programs_prefix}/include -external:I${PROGRAMS_PREFIX}/include -I${u_prefix}/include -external:I${PREFIX}/include -external:W0"
-	_libpath="-L${u_programs_prefix}/lib -L${u_prefix}/lib"
-fi
-
-##
 # Compiler and Linker flags
 #
 
@@ -40,7 +19,7 @@ if ${opt_legacy}; then
 	cxxflags=
 	ldflags=
 else
-	cppflags="-D_CRT_DECLARE_NONSTDC_NAMES"
+	cppflags="-external:W0 -external:env:INCLUDE -D_CRT_DECLARE_NONSTDC_NAMES"
 	cflags="-std:c17 -Zc:__STDC__"
 	cxxflags="-std:c++20 -Zc:__cplusplus"
 	ldflags=
@@ -99,7 +78,7 @@ esac
 
 # Add user-supplied flags
 
-cppflags="${_incpath} ${cppflags} ${CPPFLAGS}"
+cppflags="${cppflags} ${CPPFLAGS}"
 cflags="${cflags} ${CFLAGS}"
 cxxflags="${cxxflags} ${CXXFLAGS}"
-ldflags="${_libpath} ${ldflags} ${LDFLAGS}"
+ldflags="${ldflags} ${LDFLAGS}"
