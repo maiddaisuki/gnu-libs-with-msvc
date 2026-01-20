ca872c7ce1c3db4486c6fba8471c43b9595209eb
Kirill Makurin <maiddaisuki@outlook.com>
2026-01-20 19:28:03 +0900

    templates/make.sh: describe arguments to _make_test and _make_stage

--- templates/make.sh
+++ templates/make.sh
@@ -74,12 +74,38 @@ PACKAGE_build() {
 }
 
 PACKAGE_test() {
+	# By default, _make_test runs `check` target:
+	#
+	#	make -k check
+	#
+	# Some packages may have different name, such as `test`. You can pass
+	# name of the target to run as an argument:
+	#
+	#	`_make_test test` will run `make -k test`
+	#
+	# This also allows to pass options to make, but in this case you will also
+	# need to pass name of the target to run:
+	#
+	#	`_make_test -i check` will run `make -k -i check`
+	#
 	if ${MAKE_CHECK}; then
 		_make_test
 	fi
 }
 
 PACKAGE_stage() {
+	# If package supports DESTDIR mechanism, but uses different variable name,
+	# set this variable to that variable's name.
+	#local destdir_var=
+	# By default, _make_stage runs `install-strip` target:
+	#
+	#	make DESTDIR=... install-strip
+	#
+	# Some packages which do not use Automake may not support `install-strip`
+	# target. You can pass name of the target to run as an argument:
+	#
+	#	`_make_stage install` will run `make DESTDIR=... install`
+	#
 	_make_stage
 }
 
